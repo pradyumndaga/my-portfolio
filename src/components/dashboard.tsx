de8c7e3f609cbcd304e9c58c8935014abf795d89
pradyumndaga <pradyumndaga28@gmail.com>
2025-12-21 18:33:31 +0530

    height

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -3,7 +3,7 @@ import { SideMenu } from "./side-menu";
 
 export const Dashboard = () => {
     return (
-        <div className="flex h-screen w-screen overflow-hidden">
+        <div className="flex h-svh w-screen overflow-hidden">
             <div className="border-r-2 h-full"><SideMenu /></div>
             <div className="flex-1 m-4 p-2 border-2 border-gray-500 rounded-xl shadow-sm overflow-y-auto">
                 <Outlet />
